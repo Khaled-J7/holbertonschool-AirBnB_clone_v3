d3ae7584f1fa672cf3ae1a4bc81b061cfb5772a0
Khaled Jallouli <6711@holbertonstudents.com>
2024-04-19 14:54:44 +0100

    add comments

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -83,13 +83,17 @@ class TestBaseModel(unittest.TestCase):
         """Test that two BaseModel instances have different datetime objects
         and that upon creation have identical updated_at and created_at
         value."""
+        # Changed from datetime.now()
         tic = datetime.utcnow()
         inst1 = BaseModel()
+        # Changed from datetime.now()
         toc = datetime.utcnow()
         self.assertTrue(tic <= inst1.created_at <= toc)
         time.sleep(1e-4)
+        # Changed from datetime.now()
         tic = datetime.utcnow()
         inst2 = BaseModel()
+        # Changed from datetime.now()
         toc = datetime.utcnow()
         self.assertTrue(tic <= inst2.created_at <= toc)
         self.assertEqual(inst1.created_at, inst1.updated_at)
