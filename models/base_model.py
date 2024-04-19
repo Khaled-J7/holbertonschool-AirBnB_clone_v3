c7b0363e99438a61ad1b55cb417d5d579b054ad9
Khaled Jallouli <6711@holbertonstudents.com>
2024-04-19 14:44:37 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -44,7 +44,7 @@ class BaseModel:
                 self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
+            self.created_at = datetime.utcnow() if kwargs.get("created_at", None) is None else datetime.strptime(kwargs["created_at"], time)
             self.updated_at = self.created_at
 
     def __str__(self):
